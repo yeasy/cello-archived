829ce08e9e1a5d2dbc3febb6627609f80ef2fe10
Baohua Yang <yangbaohua@gmail.com>
2016-04-27 17:13:07 +0800

    Add default pages for 404 and 500 errors

--- admin/app.py
+++ admin/app.py
@@ -18,5 +18,11 @@ app.register_blueprint(cluster)
 def page_not_found(error):
     return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=app.config.get("DEBUG", True))
