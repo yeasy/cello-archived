3e98d7eae05beddcfb18644a6e781610d44a1af9
Baohua Yang <yangbaohua@gmail.com>
2016-06-05 12:09:49 +0800

    update with dev environment

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -24,7 +24,7 @@ echo "Stop all services..."
 docker-compose stop
 
 echo "Remove all services..."
-docker-compose rm -f
+docker-compose rm -f -all
 
 echo "Restart all services..."
 docker-compose up -d --no-recreate
--- scripts/start_mongoexpress.sh
+++ scripts/start_mongoexpress.sh
@@ -6,11 +6,13 @@
 BIND_ADDR=0.0.0.0
 #BIND_ADDR=127.0.0.1
 
-echo "Access port 8081 for the mongo-express UI".
+echo "Access port 8081 for the mongo-express UI"
 
 docker run -it --rm \
     --link mongo:mongo \
     --net poolmanager_default \
     -p ${BIND_ADDR}:8081:8081 \
+    -e ME_CONFIG_BASICAUTH_USERNAME=admin \
+    -e ME_CONFIG_BASICAUTH_PASSWORD=pass \
     mongo-express:0.30
 
