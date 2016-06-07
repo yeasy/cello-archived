49e030682fc2a34a61f624ee24df6cf99df2c0cb
Baohua Yang <yangbaohua@gmail.com>
2016-06-07 21:42:06 +0800

    update for release

--- admin/app.py
+++ admin/app.py
@@ -8,7 +8,7 @@ from resources.stat import stat
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object('config.ProductionConfig')
 app.config.from_envvar('POOLMANAGER_CONFIG_FILE', silent=True)
 
 app.logger.setLevel(LOG_LEVEL)
--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import logging
+import os
 from flask import Flask
 
 from common import log_handler, LOG_LEVEL
@@ -6,7 +6,7 @@ from resources import action
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object('config.ProductionConfig')
 app.config.from_envvar('POOLMANAGER_CONFIG_FILE', silent=True)
 
 app.logger.addHandler(log_handler)
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=80,
-        debug=app.config.get("DEBUG", True),
+        debug=os.environ.get('DEBUG', app.config.get("DEBUG", True)),
         threaded=True
     )
