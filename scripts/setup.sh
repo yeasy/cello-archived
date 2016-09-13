5813d788406c9dec09c249d457d6e18993c7c879
Baohua Yang <yangbaohua@gmail.com>
2016-09-13 10:55:11 +0800

    Add more comment msg to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,24 +14,24 @@ sudo pip install --upgrade pip
 
 sudo pip install --upgrade tox
 
-echo "Checking Docker-engine..."
+echo_b "Checking Docker-engine..."
 command -v docker >/dev/null 2>&1 || { echo_r >&2 "No docker-engine found, try installing"; curl -sSL https://get.docker.com/ | sh; sudo service docker restart; }
 
-echo "Add existing user to docker group"
+echo_b "Add existing user to docker group"
 sudo usermod -aG docker ${USER}
 
-echo "Checking Docker-compose..."
+echo_b "Checking Docker-compose..."
 command -v docker-compose >/dev/null 2>&1 || { echo_r >&2 "No docker-compose found, try installing"; sudo pip install -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com docker-compose; }
 
-echo "Checking local mounted database path..."
+echo_b "Checking local mounted database path..."
 [ ! -d ${DB_DIR} ] && echo_r "Local database path ${DB_DIR} not existed, creating one" && sudo mkdir -p ${DB_DIR} && sudo chown -R ${USER}:${USER} ${DB_DIR}
 
-echo "Checking local Docker image..."
+echo_b "Checking local Docker image..."
 pull_image "mongo:3.2"
 pull_image "python:3.2"
 pull_image "yeasy/nginx:latest"
 
-[ `sudo docker ps -qa|wc -l` -gt 0 ] && echo_r "Warn: existing containers may cause unpredictable failure"
+[ `sudo docker ps -qa|wc -l` -gt 0 ] && echo_r "Warn: existing containers may cause unpredictable failure, suggest to clean them using docker rm"
 
-echo_g "Please logout and login again to enable the setup."
-echo_g "It's safe to run this script repeatedly. Just re-run if the setup fails."
+echo_g "Setup done, please logout and login again."
+echo_g "It's safe to run this script repeatedly. Just re-run if it fails."
