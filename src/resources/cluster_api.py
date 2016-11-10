63b4e2ad6e826f593575db442e2eacb4b34067cf
Baohua Yang <yangbaohua@gmail.com>
2016-11-10 15:19:08 +0800

    Decouple the api and view resources

--- src/resources/cluster_api.py
+++ src/resources/cluster_api.py
@@ -278,6 +278,9 @@ def cluster_delete():
 @front_rest_v2.route('/clusters', methods=['GET', 'POST'])
 def cluster_list():
     """List clusters with the filter
+    e.g.,
+
+    GET /clusters?consensus_plugin=pbft
 
     Return objs of the clusters.
     """
@@ -291,63 +294,6 @@ def cluster_list():
     result = cluster_handler.list(filter_data=f)
     logger.error(result)
     return make_ok_response(data=result)
-
-
-bp_cluster_view = Blueprint('bp_cluster_view', __name__,
-                            url_prefix='/{}'.format("view"))
-
-
-# Return a web page with cluster info
-@bp_cluster_view.route('/cluster/<cluster_id>', methods=['GET'])
-def cluster_info_show(cluster_id):
-    logger.debug("/ cluster_info/{}?released={} action={}".format(
-        cluster_id, r.args.get('released', '0'), r.method))
-    released = (r.args.get('released', '0') != '0')
-    if not released:
-        return render_template("cluster_info.html",
-                               item=cluster_handler.get_by_id(cluster_id),
-                               consensus_plugins=CONSENSUS_PLUGINS)
-    else:
-        return render_template("cluster_info.html",
-                               item=cluster_handler.get_by_id(
-                                   cluster_id, col_name="released"),
-                               consensus_plugins=CONSENSUS_PLUGINS)
-
-
-# Return a web page with clusters
-@bp_cluster_view.route('/clusters', methods=['GET'])
-def clusters_show():
-    request_debug(r, logger)
-    show_type = r.args.get("type", "active")
-    col_filter = dict((key, r.args.get(key)) for key in r.args if
-                      key != "col_name" and key != "page" and key != "type")
-    if show_type != "released":
-        col_name = r.args.get("col_name", "active")
-    else:
-        col_name = r.args.get("col_name", "released")
-
-    if show_type == "inused":
-        col_filter["user_id"] = {"$ne": ""}
-
-    clusters = list(cluster_handler.list(filter_data=col_filter,
-                                         col_name=col_name))
-    if show_type == "active":
-        clusters.sort(key=lambda x: str(x["create_ts"]), reverse=True)
-    elif show_type == "inused":
-        clusters.sort(key=lambda x: str(x["apply_ts"]), reverse=True)
-    else:
-        clusters.sort(key=lambda x: str(x["release_ts"]), reverse=True)
-    total_items = len(clusters)
-
-    hosts = list(host_handler.list())
-    hosts_avail = list(filter(lambda e: e["status"] == "active" and len(
-        e["clusters"]) < e["capacity"], hosts))
-    return render_template("clusters.html", type=show_type, col_name=col_name,
-                           items_count=total_items, items=clusters,
-                           hosts_available=hosts_avail,
-                           consensus_plugins=CONSENSUS_PLUGINS,
-                           consensus_modes=CONSENSUS_MODES,
-                           cluster_sizes=CLUSTER_SIZES)
 
 
 # will deprecate
